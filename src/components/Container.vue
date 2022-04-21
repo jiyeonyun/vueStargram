9ce5e8466f4f5cdd62ca2f4c86f25fcc027fe27e
jiyeonyun <jiyeonyun43@gmail.com>
2022-04-21 14:24:38 +0900

    instargram data bind

---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <Post v-for="(posts,a) in postData" :key="a" :postData="postData[a]"/>
+</div>
+</template>
+
+<script>
+import Post from './Post.vue';
+export default {
+    name: 'Container-box',
+    components:{
+        Post,
+    },
+    props : {
+        postData :Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
